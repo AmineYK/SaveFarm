591ff74a99e1682ef6a07a1298f162f4d9170153
Amine YK <Amine.Youcef_Khodja@etu.sorbonne-universite.fr>
2022-07-25 22:49:57 +0200

    adding Api code

---
+++ api/main.py
@@ -0,0 +1,121 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+import math
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+MODEL = tf.keras.models.load_model("../models/PPT_1")
+MODEL_Potato = tf.keras.models.load_model("../models/Potato8")
+MODEL_Pepper = tf.keras.models.load_model("../models/Pepper2")
+MODEL_Tomato = tf.keras.models.load_model("../models/Tomato2")
+
+CLASS_NAMES = ["Potato", "Pepper", "Tomato"]
+CLASS_NAMES_Potato = ["Potato___Early_blight","Potato___Late_blight","Potato___healthy"]
+CLASS_NAMES_Pepper = ["Pepper__bell___Bacterial_spot","Pepper__bell___healthy"]
+CLASS_NAMES_Tomato = ['Tomato_Bacterial_spot',
+ 'Tomato_Early_blight',
+ 'Tomato_Late_blight',
+ 'Tomato_Leaf_Mold',
+ 'Tomato_Septoria_leaf_spot',
+ 'Tomato_Spider_mites_Two_spotted_spider_mite',
+ 'Tomato__Target_Spot',
+ 'Tomato__Tomato_YellowLeaf__Curl_Virus',
+ 'Tomato__Tomato_mosaic_virus',
+ 'Tomato_healthy']
+
+@app.get("/ping")
+async def ping():
+    return "Hello, I am alive"
+
+def read_file_as_image(data) -> np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    return image
+
+@app.post("/predict")
+async def predict(
+    file: UploadFile = File(...)
+):
+    image = read_file_as_image(await file.read())
+    img_batch = np.expand_dims(image, 0)
+    
+    predictions = MODEL.predict(img_batch)
+
+    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+    confidence = round(np.max(predictions[0]),2)
+    return {
+        'class': predicted_class,
+        'confidence': float(confidence)
+    }
+
+
+	
+@app.post("/predictPotato")
+async def predictPotato(
+    file: UploadFile = File(...)
+):
+    image = read_file_as_image(await file.read())
+    img_batch = np.expand_dims(image, 0)
+    
+    predictions = MODEL_Potato.predict(img_batch)
+
+    predicted_class = CLASS_NAMES_Potato[np.argmax(predictions[0])]
+    confidence = round(np.max(predictions[0]),2)
+    return {
+        'class': predicted_class,
+        'confidence': float(confidence)
+    }
+
+@app.post("/predictTomato")
+async def predictTomato(
+    file: UploadFile = File(...)
+):
+    image = read_file_as_image(await file.read())
+    img_batch = np.expand_dims(image, 0)
+    
+    predictions = MODEL_Tomato.predict(img_batch)
+
+    predicted_class = CLASS_NAMES_Tomato[np.argmax(predictions[0])]
+    confidence = round(np.max(predictions[0]),2)
+    return {
+        'class': predicted_class,
+        'confidence': float(confidence)
+    }
+
+
+@app.post("/predictPepper")
+async def predictPepper(
+    file: UploadFile = File(...)
+):
+    image = read_file_as_image(await file.read())
+    img_batch = np.expand_dims(image, 0)
+    
+    predictions = MODEL_Pepper.predict(img_batch)
+
+    predicted_class = CLASS_NAMES_Pepper[np.argmax(predictions[0])]
+    confidence = round(np.max(predictions[0]),2)
+    return {
+        'class': predicted_class,
+        'confidence': float(confidence)
+    }
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='localhost', port=8000)
+
